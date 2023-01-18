f721bc78b6636eda59e29e9167cc74712cdaa9bf
Guilherme Pereira <guilherme00pereira@gmail.com>
2023-01-18 20:27:00 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import Papa from "papaparse";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [file, setFile] = useState(null);
+
+  const onDrop = acceptedFiles => {
+    setFile(acceptedFiles[0]);
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  const handleUpload = () => {
+    Papa.parse(file, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      complete: (results) => {
+        setData(results.data)
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop a CSV file here, or click to select a file</p>
+        </div>
+        {file && <button onClick={handleUpload}>Upload</button>}
+        {data.map((item) => (
+            <div key={item.id}>
+              {item.name} - {item.age}
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
